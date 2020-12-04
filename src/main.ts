9833192125a953877cb8b5b320a59a9d14f6632c
Ivan Kodola <ivan.kodola@mev.com>
2020-12-04 12:28:30 +0200

    feature/auth-google: instaled amplify-auth-google

--- src/main.ts
+++ src/main.ts
@@ -13,4 +13,34 @@ if (environment.production) {
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
-Amplify.configure(aws_exports);
+
+const isLocalhost = Boolean(
+  window.location.hostname === 'localhost' ||
+  // [::1] is the IPv6 localhost address.
+  window.location.hostname === '[::1]' ||
+  // 127.0.0.1/8 is considered localhost for IPv4.
+  window.location.hostname.match(
+    /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+  )
+);
+
+const [
+  localRedirectSignIn,
+  productionRedirectSignIn,
+] = aws_exports.oauth.redirectSignIn.split(',');
+
+const [
+  localRedirectSignOut,
+  productionRedirectSignOut,
+] = aws_exports.oauth.redirectSignOut.split(',');
+
+const updatedAwsConfig = {
+  ...aws_exports,
+  oauth: {
+    ...aws_exports.oauth,
+    redirectSignIn: isLocalhost ? localRedirectSignIn : productionRedirectSignIn,
+    redirectSignOut: isLocalhost ? localRedirectSignOut : productionRedirectSignOut,
+  }
+};
+
+Amplify.configure(updatedAwsConfig);
