1d75edd2432ae255f49696f08b2417d0bba36eb0
Ivan Kodola <ivan.kodola@mev.com>
2020-12-04 15:38:02 +0200

    feature/auth: hide phone fields on sign-up form

--- src/app/auth/components/authenticator/authenticator.component.ts
+++ src/app/auth/components/authenticator/authenticator.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { AuthState } from '@aws-amplify/ui-components';
+import { FormFieldTypes } from '@aws-amplify/ui-components';
 
 @UntilDestroy()
 @Component({
@@ -11,11 +12,30 @@ import { AuthState } from '@aws-amplify/ui-components';
 })
 export class AuthenticatorComponent implements OnInit {
   initialAuthState: AuthState = AuthState.SignIn;
+  signUpFormFields: FormFieldTypes;
 
   constructor(private readonly activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.buildSignUpFormFields();
     this.listenRouterData();
+  }
+
+  private buildSignUpFormFields(): void {
+    this.signUpFormFields = [
+      {
+        type: 'email',
+        label: 'Email Address *',
+        placeholder: 'Email',
+        required: true,
+      },
+      {
+        type: 'password',
+        label: 'Password *',
+        placeholder: 'Password',
+        required: true,
+      },
+    ];
   }
 
   private listenRouterData(): void {
