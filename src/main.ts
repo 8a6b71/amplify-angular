eb56973f98767fa3fbfb333700d78972ff2e44d1
Ivan Kodola <ivan.kodola@mev.com>
2020-12-10 17:50:10 +0200

    feature/auth-google: refactored main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import aws_exports from './aws-exports';
 import { Auth } from '@aws-amplify/auth';
-
 import { AppModule } from './app/app.module';
+import awsconfig from './aws-exports';
 import { environment } from './environments/environment';
+import { getEnvAmplifyConfig } from './helpers/amplifyConfigConstructor';
 
 if (environment.production) {
   enableProdMode();
@@ -13,33 +13,4 @@ if (environment.production) {
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
-const isLocalhost = Boolean(
-  window.location.hostname === 'localhost' ||
-  // [::1] is the IPv6 localhost address.
-  window.location.hostname === '[::1]' ||
-  // 127.0.0.1/8 is considered localhost for IPv4.
-  window.location.hostname.match(
-    /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-  )
-);
-
-const [
-  localRedirectSignIn,
-  productionRedirectSignIn,
-] = aws_exports.oauth.redirectSignIn.split(',');
-
-const [
-  localRedirectSignOut,
-  productionRedirectSignOut,
-] = aws_exports.oauth.redirectSignOut.split(',');
-
-const updatedAwsConfig = {
-  ...aws_exports,
-  oauth: {
-    ...aws_exports.oauth,
-    redirectSignIn: isLocalhost ? localRedirectSignIn : productionRedirectSignIn,
-    redirectSignOut: isLocalhost ? localRedirectSignOut : productionRedirectSignOut,
-  }
-};
-
-Auth.configure(updatedAwsConfig);
+Auth.configure(getEnvAmplifyConfig(awsconfig));
